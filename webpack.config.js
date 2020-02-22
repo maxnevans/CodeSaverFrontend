beebd817d76b044390226c3029f8d2b909dd0dcf
Maxim Shcherbakov <maxnevans@gmail.com>
2020-02-22 14:04:26 +0300

    small tweaks have been made to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,73 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-    devtool : 'inline-source-map',
-    mode: "development",
-    entry: ["@babel/polyfill", "./src/bootstrap.jsx"],
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "app.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                include: path.resolve(__dirname, "src"),
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.module\.s(a|c)ss$/,
-                loader: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true,
-                            sourceMap: true
-                        }
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true
-                        }
+module.exports = (env, argv) => {
+    const devMode = argv.mode !== 'production';
+
+    return {
+        devtool : 'inline-source-map',
+        mode: "development",
+        entry: {
+            polyfill: '@babel/polyfill',
+            app: "./src/bootstrap.jsx",
+        },
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: devMode ? "[name].js" : "[name].[hash].js"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    include: path.resolve(__dirname, "src"),
+                    use: {
+                        loader: "babel-loader"
                     }
-                ]
-            },
-            {
-                test: /\.s(a|c)ss$/,
-                exclude: /\.module.(s(a|c)ss)$/,
-                loader: [
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true
+                },
+                {
+                    test: /\.(css|sass|scss)$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                importLoaders: 2,
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                plugins: () => [
+                                    require('autoprefixer')
+                                ],
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
                         }
-                    }
-                ]
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
+                    ]
+                },
+            ]
+        },
+        resolve: {
+            extensions: ['.js', '.jsx'],
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: './src/index.html'
+            }),
+            new MiniCssExtractPlugin({
+                filename: devMode ? "[name].css" : "[name].[hash].css",
+                chunkFilename: '[id].css'
+            })
         ]
-    },
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].css'
-        })
-    ]
+    };
 };
